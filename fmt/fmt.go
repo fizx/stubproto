e19a72ba56a2ef86b7195544c0f3563ac172b47b
Kyle Maxwell <kyle.maxwell@reddit.com>
2021-02-15 21:47:07 -0800

    stubs

--- fmt/fmt.go
+++ fmt/fmt.go
@@ -2,10 +2,8 @@ package fmt
 
 import (
 	"errors"
-	"fmt"
 )
 
 func Errorf(args ...interface{}) error {
-	str := fmt.Sprintf(args[0].(string), args[1:])
-	return errors.New(str)
+	return errors.New("unknown error, message stubbed out")
 }
